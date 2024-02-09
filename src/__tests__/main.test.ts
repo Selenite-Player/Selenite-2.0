60111f92e657c0d765873e55ee2874567e905271
mhanki <meike.hankewicz@protonmail.com>
2024-02-09 19:55:50 +0100

    Update pipeline and delete empty tests

--- src/__tests__/main.test.ts
+++
@@ -1,9 +0,0 @@
-/* 
-  * Receive and handle play/pause event
-  * Receive and handle skip event
-  * Receive and handle prev event
-  * Receive and handle seek event
-  * Receive and handle shuffle event
-  * Receive and handle repeat event
-  * Receive and handle like event
-*/
